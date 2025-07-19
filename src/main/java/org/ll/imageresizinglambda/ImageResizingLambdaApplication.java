e0ed676e5fe2dbe67d22f4a9a63d82a30b859d47
seonghyeok <chosh2001@naver.com>
2025-07-20 00:10:03 +0900

    init

---
+++ src/main/java/org/ll/imageresizinglambda/ImageResizingLambdaApplication.java
@@ -0,0 +1,13 @@
+package org.ll.imageresizinglambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizingLambdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageResizingLambdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ll/imageresizinglambda/ImageResizingLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ll.imageresizinglambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageResizingLambdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
